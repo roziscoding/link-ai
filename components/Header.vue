c793098a69a6473d9cbb7ff0dd68234db34d119f
Rogerio Munhoz <roz@rjmunhoz.me>
2024-02-14 14:06:51 -0300

    feat: add background image

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,9 @@
 const config = useRuntimeConfig();
 </script>
 <template>
-  <div class="flex flex-col items-center">
+<div class="flex flex-col items-center p-5">
     <img class="aspect-square rounded-full h-28 xl:h-48" :src="config.avatarUrl" alt="Profile picture" />
-    <h1 class="xl:text-3xl text-xl w-11/12 text-white text-center xl:mt-5">{{ config.title }}</h1>
-    <p class="xl:text-2xl text-l w-11/12 text-gray-500 text-center">{{ config.subtitle }}</p>
+    <h1 class="xl:text-3xl text-xl w-11/12 text-white font-bold text-center xl:mt-5">{{ config.title }}</h1>
+    <p class="xl:text-lg text-sm w-11/12 font-bold text-white text-center">{{ config.subtitle }}</p>
   </div>
 </template>
